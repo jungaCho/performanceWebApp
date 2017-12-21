b788a82663a65b0d905f93ccaa9be7a651e0999b
USER <USER@USER-PC>
2017-12-21 19:44:46 +0900

    ad - FindId

--- performanceWebApp/src/controller/member/LoginCommand.java
+++ performanceWebApp/src/controller/member/LoginCommand.java
@@ -38,7 +38,6 @@ public class LoginCommand implements Command {
 				forward.setRedirect(false);
 				return forward;
 				
-				
 			} else {
 				// 로그인 원래화면으로 계속 이동한다.
 				forward.setPath("/loginForm.do");
